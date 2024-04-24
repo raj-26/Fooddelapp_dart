75f7d2e2d0057203c5aef7cf833aa517a4ac7001
aditya <adityarajparashar5@gmail.com>
2024-04-24 23:32:43 +0530

    food

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,384 @@
+import 'package:fooddelapp_dart/models/food.dart';
+
+class Restaurant {
+  // list of food menu
+  final List<Food> _menu = [
+// burgers
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+// salads
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+//sides
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+// desserts
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+// drinks
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+
+    Food(
+      name: "Classic Cheese Burger",
+      description:
+          "a A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "images/burger.jpg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra cheese", price: 0.99),
+      ],
+    ),
+  ];
+
+  // G E T T E R S
+
+  List<Food> get menu => _menu;
+
+  // O P E R A T I O N S
+
+  // add to cart
+
+  //remove from cart
+
+  // get total price of cart
+
+  // get total number of items in cart
+
+  // clear cart
+
+  // H E L P E R S
+
+  // generate a receipt
+
+  // format double value into money
+
+  // format list of addons
+}
